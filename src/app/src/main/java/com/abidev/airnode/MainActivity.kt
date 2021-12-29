7ce46e6ca5410be7116ff83ba188fe318cb1d556
Abidán Brito <abidan.brito@gmail.com>
2021-12-29 10:37:29 +0100

    feat: added font family, created a separate appbar layout and fixed icons

--- src/app/src/main/java/com/abidev/airnode/MainActivity.kt
+++ src/app/src/main/java/com/abidev/airnode/MainActivity.kt
@@ -2,10 +2,20 @@ package com.abidev.airnode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abidev.airnode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Inflate the layout and get an instance (View Binding)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set custom Toolbar
+        setSupportActionBar(binding.appbarLayout.toolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
     }
 }
